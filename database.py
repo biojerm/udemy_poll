6884c110f7eabef813ca1fb0394f72c36005249d
Jeremy Labarge <jlabarge@scharp.org>
2021-08-06 16:21:17 -0700

    Add functionality to grab random voter

--- database.py
+++ database.py
@@ -18,6 +18,9 @@ WHERE polls.id = (
     SELECT id FROM polls ORDER BY id DESC limit 1
 );"""
 
+SELECT_RANDOM_VOTE = (
+    "SELECT * FROM votes WHERE option_id = %s ORDER BY RANDOM() limit 1;"
+)
 INSERT_POLL_RETURN_ID = (
     "INSERT INTO polls (title, owner_username) VALUES (%s, %s) RETURNING id;"
 )
@@ -63,7 +66,8 @@ def get_poll_and_vote_results(connection, poll_id):
 def get_random_poll_vote(connection, option_id):
     with connection:
         with connection.cursor() as cursor:
-            pass
+            cursor.execute(SELECT_RANDOM_VOTE, (option_id,))
+            return cursor.fetchone()
 
 
 def create_poll(connection, title, owner, options):
